67cd71a6216c08b617bacf6392e0837a26e676bc
wh0ohm <wararat.boonchieng@gmail.com>
2024-12-24 15:51:55 +0700

    12-1 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
+long long int fibonacci(long long int n);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
+
+
+
+long long int fibonacci(long long int n){
+        if(n>1){
+            return fibonacci(n-1)+fibonacci(n-2);
+        }else {
+            return n;
+        }
+    }
